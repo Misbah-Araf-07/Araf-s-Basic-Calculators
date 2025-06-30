b4e9335b77528dcf5ceca9cd9b03892b2d8cbd06
Misbah Ahmed Araf <arafallen0@gmail.com>
2025-07-01 00:23:32 +0600

    Added C Files

---
+++ Araf's Basic Calculator Version 1.0.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    printf("\t\t\t\t \"Misbah Ahmed Araf's Basic Calculator Version 1.0\" \t\n\n\n");
+    printf("\t\tPlease Type Number_1 ,then the operator you like, followed by Number_2\n\n\n");
+
+    double Number_1=0 , Number_2=0 ;
+    char Operator = 0, Confirmation=0 , Comparer =0;
+
+    scanf("%lf %c %lf",&Number_1 ,&Operator,&Number_2);
+    switch (Operator)
+    {
+
+     case '+': printf("The Added Value is= %.1lf + %.1lf= %.1lf",Number_1,Number_2,(Number_1+Number_2));
+     break;
+     case '-': printf("The Subtracted Value is= %.1lf - %.1lf= %.1lf",Number_1,Number_2,(Number_1-Number_2));
+     break;
+     case '/': printf("The Divided Value is= %.1lf/%.1lf= %d and the Remainder is : %.0lf",Number_1,Number_2,((float)(Number_1/Number_2)),((int)Number_1 % (int) Number_2));
+     break;
+     case '*': printf("The Multiplied Value is= %.1lf * %.1lf= %.1lf",Number_1,Number_2,(Number_1*Number_2));
+     break;
+
+
+    default : printf("Calculation Error\n\n\n");
+
+    }
+
+    int C1,C2;
+
+    printf("\nDo you want to Compare numbers ? Press X for Yes or Y for No \n\n\t");
+
+    scanf("%c",&Confirmation);
+
+    switch (Confirmation)
+    {
+
+     case 'X': printf("Please Type the values that you wanna compare:");
+     scanf("%lf %lf",&C1,&C2);
+     printf("The Typed Values are :%.1lf , %.1lf.",C1,C2);
+     break;
+
+     case 'Y' : printf("Calculation Completed\n\n\n");
+     break;
+    }
+
+    printf("\n\n\n\t\t___Thank you For Using , I'm bringing upgrades in the Version 1.5___\n\n");
+    printf("\n\n\n_____________________________________________________");
+
+    return 0;
+}
---
+++ Araf's Basic Calculator Version 1.5.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main ()
+
+{
+    printf("\t\t\t\t \"Misbah Ahmed Araf's Basic Calculator Version 1.5\" \t\n\n\n");
+
+    char M=0,N=0,A,B,C,D ;
+    double P=0,Q=0;
+
+    printf("Please select the type of operation you want: \n\tPress A for(+)\n\tPress B for(-)\n\tPress C for(x)\n\tPress D for(/)\n\n");
+    scanf("%c",&N);
+
+    switch (N)
+    {
+
+    if
+        (N==A)
+        printf("Please type the two numbers you want to add to each other\n\n");
+
+    else if
+        (N==B)
+        printf("Please type the two numbers you want to subtract from (1st number-2nd number)\n\n");
+
+    else if
+        (N==C)
+        printf("Please type the two numbers you want to multiply with each other\n\n");
+
+    else if
+        (N==D)
+
+        printf("Please type the two numbers you want a division from (1st number / 2nd number)\n\n");
+
+    break;
+    default: printf("\t\t\t______________________Calculations Execution done______________________\n\n");
+
+
+
+
+    printf("Do you want to Compare numbers ?\n\n\n");
+
+    scanf("%c",&N);
+
+
+
+        printf("\t\t\t\t_Thank you for Using_\n\n____________________________________");
+
+
+
+    ;
+
+    }
+    return 0;
+}
